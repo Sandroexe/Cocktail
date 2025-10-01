2536de7d340b06e8a701c4c557abeb3dd0935a9d
Sandro <sandro.exenberger@gmail.com>
2025-10-01 12:18:15 +0200

    Aufruf CocktailDB

--- cocktail.py
+++ cocktail.py
@@ -1,3 +1,13 @@
 # Cocktail - API
 
 print("Cocktail - API")
+
+# Bibliotheken importieren
+import requests
+
+
+# Programm Code
+
+response = requests.get("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=Margarita")
+data = response.json()
+print(data)
