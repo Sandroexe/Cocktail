59b7dfa31e7fef218d467487286e9b30a085251d
Sandro <sandro.exenberger@gmail.com>
2025-10-14 21:28:28 +0200

    Test

--- cocktail.py
+++ cocktail.py
@@ -5,18 +5,5 @@ print("Cocktail - API")
 # Bibliotheken importieren
 import requests
 
-'''
 
-# Programm Code
-
-# Nutzer nach dem Getränk fragen
-drink = input("Welches Getränk möchtest du suchen? ")
-
-# Variable in die URL einfügen
-response = requests.get(f"https://www.thecocktaildb.com/api/json/v1/1/search.php?s={drink}")
-
-# Ausgabe (z.B. rohe JSON-Daten)
-print(response.json())
-
-'''
 
